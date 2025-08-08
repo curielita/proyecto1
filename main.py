ea6c93ba578290d364caa1a2450999c1a4afaeff
Juanchi Gonzalez <juanchigonzalez@MacBook-Pro-de-Juanchi.local>
2025-08-07 20:40:44 -0400

    LLamado a main de la funcion principal

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hola mundo esta es la rama de juan")
+from metr_art import MetroArt
+from metr_art import MetroArt
+
+#    Punto de entrada del sistema MetroArt.
+#    Inicializa la galería interactiva.
+def main():
+    galeria = MetroArt()
+    galeria.iniciar()
+
+if __name__ == "__main__":
+    main()
