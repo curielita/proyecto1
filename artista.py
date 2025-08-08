29f4eb49f7f81edbbb5a27a7a003b3f2d0132978
Juan Gonzalez <j.gonzalez2019@correo.unimet.edu.ve>
2025-08-08 19:56:51 -0400

    Main final 2

--- artista.py
+++ artista.py
@@ -2,30 +2,11 @@
 class Artista:
 
     def __init__(self, nombre, nacionalidad, nacimiento, fallecimiento):
-        if nombre != "":
             self.nombre = nombre
-        else:
-            self.nombre = "Desconocido"
-
-        if nacionalidad != "":
             self.nacionalidad = nacionalidad
-        else:
-            self.nacionalidad = "Desconocida"
-
-        if nacimiento != "":
             self.nacimiento = nacimiento
-        else:
-            self.nacimiento = "N/A"
-
-        if fallecimiento != "":
             self.fallecimiento = fallecimiento
-        else:
-            self.fallecimiento = "N/A"
-
     def descripcion(self):
-        """
-        Devuelve una cadena con la descripción del artista.
-        """
         return (
             f"Artista: {self.nombre}\n"
             f"Nacionalidad: {self.nacionalidad}\n"
